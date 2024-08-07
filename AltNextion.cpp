deac7d2d20abab1904b9fea128af309c51fa82a9
Albert Galimov <psialt@mail.ru>
2024-08-08 00:31:49 +0300

    smth meaningful

--- AltNextion.cpp
+++ AltNextion.cpp
@@ -3,8 +3,111 @@
 #include "AltNextion.hpp"
 
 namespace AltNextion {
-const Message *Display::listen(unsigned long timeout_ms=1000) {
+const uint8_t endbyte = 0xff;
 
+bool Display::listen(long timeout_ms) {
+    unsigned long last_ms = millis();
+    const int delay_ms = 10;
+    while( serial.available()>0 & timeout_ms>delay_ms ){
+        delay(delay_ms);
+        unsigned long m = millis();
+        timeout_ms -= (m-last_ms);
+        last_ms=m;
+        serial.setTimeout( timeout_ms > delay_ms ? timeout_ms : delay_ms );
+        size_t rd = serial.readBytesUntil(endbyte, &rdbuf[rdbuf_len], sizeof(rdbuf)-rdbuf_len);
+        for(int i=rdbuf_len; i<(rdbuf_len+rd); i++) {
+            if( rdbuf[i] == endbyte )
+                endcount++;
+        }
+        rdbuf_len += rd;
+        if( rdbuf_len >= sizeof(rdbuf) )
+            rdbuf_len = sizeof(rdbuf)-1;
+        if( endcount > 3 )
+            abort(); //omg somethign wrong happend
+        if( endcount == 3 ) {
+            msg.len = rdbuf_len - endcount;
+            msg.payload_u8[msg.len-1] = 0;
+
+            dispatchEvent();
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool Display::sendCommand(const char *cmd, int len) {
+    if( len < 0 )
+        len = strlen(cmd);
+    serial.write(cmd, len);
+    serial.write(0xFF);
+    serial.write(0xFF);
+    serial.write(0xFF);
+    return true; //serial.write has "no real error management" lol..
+}
+
+
+bool Display::getComponentValue(const char *name, uint32_t &val) {
+    MessageWatcherNumber w(*this);
+    int l = strlen(name);
+    char buf[4 + l + 4 + 1] = "get ";
+    strcat(buf, name);
+    strcat(buf, ".val");
+    sendCommand(buf);
+    for(int i=0; i<10 && !w.hadMessage(); i++) {
+        delay(10);
+        listen(90);
+    }
+    if( w.hadMessage() ) {
+        val = w.getValue();
+        return true;
+    }
+    return false;
+}
+
+bool Display::getComponentValue(const char *name, String &val) {
+    MessageWatcherString w(*this);
+    int l = strlen(name);
+    char buf[4 + l + 4 + 1] = "get ";
+    strcat(buf, name);
+    strcat(buf, ".val");
+    sendCommand(buf);
+    for(int i=0; i<10 && !w.hadMessage(); i++) {
+        delay(10);
+        listen(90);
+    }
+    if( w.hadMessage() ) {
+        val = w.getValue();
+        return true;
+    }
+    return false;
+
+}
+
+
+// String getValue = "get "+ component +".val";//Get componetn value
+//     unsigned int value = 0;
+//   sendCommand(getValue.c_str());
+//   uint8_t temp[8] = {0};
+//   // nextion->setTimeout(20);
+//   if (sizeof(temp) != nextion->readBytes((char *)temp, sizeof(temp))){
+//     return -1;
+//   }//end if
+//   if((temp[0]==(0x71))&&(temp[5]==0xFF)&&(temp[6]==0xFF)&&(temp[7]==0xFF)){
+//     value = (temp[4] << 24) | (temp[3] << 16) | (temp[2] << 8) | (temp[1]);//Little-endian convertion
+//   }//end if
+//   return value;
+// }//get_component_value */
+
+
+void Display::dispatchEvent() {
+    MessageWatcher *w = watchers_head;
+    while( w ) {
+        w->checkMessage(msg);
+        w = w->watcher_next;
+    }
+    endcount = 0;
+    rdbuf_len = 0;
 }
 
 void Display::addWatcher(MessageWatcher *w) {
@@ -27,7 +130,50 @@ void Display::rmWatcher(MessageWatcher *w) {
 }
 
 bool MessageWatcher::checkMessage(const Message &msg) {
-    return _code == msg.code;
+    if( code == msg.code ) {
+        if( cb )
+            cb(msg);
+        hadMessageFlag=true;
+    }
+    return code == msg.code;
+}
+
+MessageWatcherNumber::MessageWatcherNumber(Display &d) : MessageWatcher(d, 0x71) {
+}
+bool MessageWatcherNumber::checkMessage(const Message &msg) {
+    if( MessageWatcher::checkMessage(msg ) ) {
+        val = msg.payload_u32[0];
+        return true;
+    }
+    return false;
+}
+
+MessageWatcherString::MessageWatcherString(Display &d) : MessageWatcher(d, 0x70) {
+
+}
+
+bool MessageWatcherString::checkMessage(const Message &msg) {
+    if( MessageWatcher::checkMessage(msg ) ) {
+        val = String((const char*)msg.payload_u8); //-1 for the code
+        return true;
+    }
+    return false;
+}
+
+bool MessageWatcherSendme::checkMessage(const Message &msg) {
+    if( code == msg.code && page_id == msg.payload_u8[0] ) {
+        MessageWatcher::checkMessage(msg );
+        return true;
+    }
+    return false;
+}
+
+bool MessageWatcherTouchEvent::checkMessage(const Message &msg) {
+    if( code == msg.code && page_id == msg.payload_u8[0] && component_id == msg.payload_u8[1] ) {
+        MessageWatcher::checkMessage(msg );
+        return true;
+    }
+    return false;
 }
 
 };
--- AltNextion.hpp
+++ AltNextion.hpp
@@ -2,7 +2,6 @@
 #ifndef __ALTNEXTION_H__
 #define __ALTNEXTION_H__
 #include <stdint.h>
-#include <HardwareSerial.h>
 
 namespace AltNextion {
 
@@ -10,6 +9,7 @@ namespace AltNextion {
 #define MAX_MESSAGE_WATCHERS 10
 
 struct __attribute__ ((packed)) Message {
+    uint32_t len;
     uint8_t code;
     union {
         uint8_t payload_u8[16];
@@ -18,6 +18,7 @@ struct __attribute__ ((packed)) Message {
     };
     // The "ff ff ff" (do not contain here)
 };
+class MessageWatcher;
 
 // class that manages Nextion single display connection
 class Display {
@@ -25,9 +26,14 @@ public:
     Display(Stream &stream) : serial(stream) {
     }
 
-    const Message *listen(unsigned long timeout_ms=1000);
+    bool listen(long timeout_ms=1000);
+    bool getComponentValue(const char *name, uint32_t &val);
+    bool getComponentValue(const char *name, String &val);
+    bool sendCommand(const char *cmd, int len=-1);
+
     friend class MessageWatcher;
 protected:
+    void dispatchEvent();
     void addWatcher(MessageWatcher *w);
     void rmWatcher(MessageWatcher *w);
 private:
@@ -36,53 +42,74 @@ private:
         Message msg;
         uint8_t rdbuf[ALTNETION_MAX_MESSAGE_LEN];
     };
-    int rdbuf_len=0;
+    int rdbuf_len=0, endcount=0;
     MessageWatcher *watchers_head = NULL;
     
 };
 
 // A class that watches for specific message prefix for you, so you can add listeners
+typedef void (*watcherCallbackPtr)(const Message &msg);
 class MessageWatcher {
 public:
-    MessageWatcher(Display &d, uint8_t code) : _d(d), _code(code) {
-        _d.addWatcher(this);
+    MessageWatcher(Display &_d, uint8_t _code) : d(_d), code(_code) {
+        d.addWatcher(this);
     }
     virtual ~MessageWatcher() {
-        _d.rmWatcher(this);
+        d.rmWatcher(this);
     }
+    void setCallback(watcherCallbackPtr _cb) { cb = _cb; }
 
     virtual bool checkMessage(const Message &msg);
+    bool hadMessage() const { return hadMessageFlag; }
 private:
     friend class Display;
-    Display &_d;
+    Display &d;
     MessageWatcher *watcher_prev = NULL, *watcher_next = NULL;
-    uint8_t _code;
+    watcherCallbackPtr cb;
+protected:
+    uint8_t code;
+    bool hadMessageFlag = false;
 };
 
 class MessageWatcherNumber : public MessageWatcher {
 public:
-    MessageWatcherNumber(Display &d) : MessageWatcher(d, 0x71) {}
+    MessageWatcherNumber(Display &d);
+
+    bool checkMessage(const Message &msg);
+    uint32_t getValue() { return val; }
+private:
+    uint32_t val=-1;
 };
+
 class MessageWatcherString : public MessageWatcher {
 public:
-    MessageWatcherString(Display &d) : MessageWatcher(d, 0x70) {}
+    MessageWatcherString(Display &d);
+
+    bool checkMessage(const Message &msg);
+    String getValue() { return val; }
+private:
+    String val;
 };
 
 class MessageWatcherSendme : public MessageWatcher {
 public:
-    MessageWatcherSendme(Display &d, int page_id=-1) : MessageWatcher(d, 0x66), _page_id(page_id) {}
+    MessageWatcherSendme(Display &d, int _page_id=-1) : MessageWatcher(d, 0x66), page_id(_page_id) {}
+
+    bool checkMessage(const Message &msg);
 private:
-    int _page_id=-1;
+    int page_id=-1;
 };
 
 // Watch 
 class MessageWatcherTouchEvent : public MessageWatcher {
 public:
-    MessageWatcherTouchEvent(Display &d, int page_id=-1, int component_id=-1) :
-        MessageWatcher(d, 0x65), _page_id(page_id), _component_id(component_id) {}
+    MessageWatcherTouchEvent(Display &d, int _page_id=-1, int _component_id=-1) :
+        MessageWatcher(d, 0x65), page_id(_page_id), component_id(_component_id) {}
+
+    bool checkMessage(const Message &msg);
 private:
-    int _page_id=-1;
-    int _component_id=-1;
+    int page_id=-1;
+    int component_id=-1;
 };
 
 
